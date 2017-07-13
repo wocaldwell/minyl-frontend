056c3eb4b5eed37aa23ed858d38b70e0294c0e52
William Caldwell <wocaldwell@gmail.com>
2017-07-13 16:33:29 -0500

    changed splash page quote

--- minyl_frontend/general/SplashCtrl.js
+++ minyl_frontend/general/SplashCtrl.js
@@ -1,5 +1,24 @@
-app.controller('SplashController', function($scope, $http, $location, AuthFactory) {
+app.controller('SplashController', function($scope, $http, $location, apiUrl) {
 
+    function getAllQuotes() {
+        return $http({
+            url: `${apiUrl}/quotes/`
+        })
+    };
 
+    function selectRandomQuote(quoteArray) {
+        let quoteObject = quoteArray[Math.floor(Math.random() * quoteArray.length)];
+        return quoteObject;
+    };
 
+    getAllQuotes()
+    .then(function(returnedCollection){
+        let allQuotes = returnedCollection.data;
+        return allQuotes;
+    }).then(function(returnedArray){
+        let randomQuote = selectRandomQuote(returnedArray);
+        $scope.displayQuote = randomQuote.quote;
+        $scope.displayAuthor = randomQuote.author.split(',')[0];
+        return randomQuote;
+    });
 });
