9271f17975029a8a6f3dd27b23caed8cd24067ac
William Caldwell <wocaldwell@gmail.com>
2017-09-12 16:20:19 -0500

    data from a barcode image is being read.

--- minyl_frontend/members/AddBarcodeCtrl.js
+++ minyl_frontend/members/AddBarcodeCtrl.js
@@ -4,5 +4,21 @@ app.controller('AddBarcodeController', function($scope, ReleaseFactory) {
     $("#the-file-input").change(function() {
         // will log a FileList object, view gifs below
         console.log(this.files);
+        let barcodeImage = URL.createObjectURL(this.files[0]);
+        Quagga.decodeSingle({
+            decoder: {
+                readers: ["upc_reader", "upc_e_reader"] // List of active readers
+            },
+            locate: true, // try to locate the barcode in the image
+            patchSize: "x-large",
+            src: barcodeImage // or 'data:image/jpg;base64,' + data
+        }, function(result){
+            if(result.codeResult) {
+                console.log("result", result.codeResult.code);
+            } else {
+                console.log(result);
+                console.log("not detected");
+            }
+        });
     });
 });
